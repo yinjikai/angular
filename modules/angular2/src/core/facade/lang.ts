2e1468318da2153dc6d9f39336a252fc2ac8b844
vsavkin <avix1000@gmail.com>
2015-09-11 15:38:09 -0700

    refactor(async): replace RxJS with RxNext
    Closes #3110

--- modules/angular2/src/core/facade/lang.ts
+++ modules/angular2/src/core/facade/lang.ts
@@ -336,11 +336,12 @@ export function setValueOnPath(global: any, path: string, value: any) {
   var obj: any = global;
   while (parts.length > 1) {
     var name = parts.shift();
-    if (obj.hasOwnProperty(name)) {
+    if (obj.hasOwnProperty(name) && obj[name]) {
       obj = obj[name];
     } else {
       obj = obj[name] = {};
     }
   }
+
   obj[parts.shift()] = value;
 }
